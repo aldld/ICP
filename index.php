70f4c2280cfcff1013578a661631c4e57fd66e42
Eric Bannatyne <eric.bannatyne@gmail.com>
2011-10-01 11:33:04 -0700

    Modified routing in index.php

--- index.php
+++ index.php
@@ -11,31 +11,27 @@ set_include_path(dirname(__FILE__));
 
 require_once 'config.php';
 require_once 'include/auth.php';
+require_once 'include/page.php';
 
 if (!Auth::loggedIn()) {
 	// User is not logged in
 	
 	if (isset($_GET['action'])) {
-		if ($_GET['action'] == 'login') {
-			// TODO: Display login page
-		} else {
-			header('Location: index.php');
-		}
-	} else {
-		// TODO: Display signup page
-	}
+		if ($_GET['action'] == 'login')
+			loadPage('login');
+		else
+			// Redirect to signup page
+			header('Location: ' . BASE_URL . 'index.php');
+	} else
+		loadPage('signup');
+	
 } else {
 	// User is logged in
+	echo 'You are logged in!';
 	
-	if (isset($_GET['action'])) {
-		$page = $_GET['action'];
-		
-		// To prevent users from accessing files outside of pages/
-		if (strpos($action, '/') !== false) exit();
-		
-		require_once 'pages/' . $page . '.php';
-		echo $action;
-	} else {
-		// TODO: Display dashboard
-	}
+	if (isset($_GET['action']))
+		loadPage($_GET['action']);
+	else
+		loadPage('dashboard');
+	
 }
